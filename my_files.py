d0550f0829f6658a7a20d4605376581f276f10cc
ADI10HERO <39651310+ADI10HERO@users.noreply.github.com>
2019-02-24 17:09:39 +0530

    Add files via upload

---
+++ my_files.py
@@ -0,0 +1,49 @@
+import numpy as np
+import h5py
+
+
+def sigmoid(Z):
+    
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+    
+    return A, cache
+
+def relu(Z):
+    A = np.maximum(0,Z)
+    cache = Z 
+    return A, cache
+
+
+def relu_backward(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)      
+    dZ[Z <= 0] = 0
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    Z = cache
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    return dZ
+
+#copied learn later
+def load_data():
+    train_dataset = h5py.File('train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])
+
+    test_dataset = h5py.File('test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])
+
+    classes = np.array(test_dataset["list_classes"][:])
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+#XDXD
+
+
+
